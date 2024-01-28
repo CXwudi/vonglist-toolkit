34b28903618b972b12c433a13571c6afa3672f3a
CXwudi <cx2010017@gmail.com>
2024-01-28 16:34:13 -0500

    :sparkles: Add ItemReader interface to shared services
    This commit introduces a new interface, `ItemReader<T>`, under the shared services of the `nico2vocadb-songlist-exporter-core` module. This interface is designed to read a list of items from an unspecified source.
    
    It provides two methods:
    
    - `readAll()`: This method reads all items and returns them as a list. It uses a `do-while` loop to continuously add items to the list until no more items are found.
    
    - `read()`: This method reads the next item and returns it. If no more items are available, it returns `null`.
    
    These methods aim to provide a general mechanism for reading items from a source, making it a reusable component across different parts of the application where similar functionality is required.

---
+++ core-logic/nico2vocadb-songlist-exporter-core/src/main/kotlin/mikufan/cx/vtool/service/api/shared/ItemReader.kt
@@ -0,0 +1,19 @@
+package mikufan.cx.vtool.service.api.shared
+
+/**
+ * Read a list of item from somewhere
+ */
+interface ItemReader<T> {
+  fun readAll(): List<T> = buildList {
+    do {
+      val item = read()
+      if (item != null) add(item)
+    } while (item != null)
+  }
+
+  /**
+   * read next item, return null if no more item
+   * @return T? the next item, or null if no more item
+   */
+  fun read(): T?
+}
