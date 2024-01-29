4f4d8cf5b1965375a1619c255a72e856dce76767
CXwudi <cx2010017@gmail.com>
2024-01-29 02:41:21 -0500

    :sparkles: Add main method
    This commit introduces two new files `Nico2VocaDbSongListExporter.kt` and `Main.kt` in the `nico2vocadb-songlist-exporter` module.
    
    `Nico2VocaDbSongListExporter.kt` is the entry point of the application marked with `@SpringBootApplication` and `@ConfigurationPropertiesScan`. This class will be responsible for the initialization and configuration of the Spring Boot application.
    
    `Main.kt` includes the `main` function, which is the entry point of the Kotlin application. It calls `runApplication<Nico2VocaDbSongListExporter>(*args)` to start the Spring Boot application.

---
+++ app/nico2vocadb-songlist-exporter/src/main/kotlin/mikufan/cx/vtool/app/n2vex/Main.kt
@@ -0,0 +1,7 @@
+package mikufan.cx.vtool.app.n2vex
+
+import org.springframework.boot.runApplication
+
+fun main(args: Array<String>) {
+  runApplication<Nico2VocaDbSongListExporter>(*args)
+}
---
+++ app/nico2vocadb-songlist-exporter/src/main/kotlin/mikufan/cx/vtool/app/n2vex/Nico2VocaDbSongListExporter.kt
@@ -0,0 +1,8 @@
+package mikufan.cx.vtool.app.n2vex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+class Nico2VocaDbSongListExporter
