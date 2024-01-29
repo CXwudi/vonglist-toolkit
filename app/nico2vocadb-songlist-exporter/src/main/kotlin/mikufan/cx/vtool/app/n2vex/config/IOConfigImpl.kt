b40d4f4458469de468a7315c52dd76956baf4cb5
CXwudi <cx2010017@gmail.com>
2024-01-29 02:41:49 -0500

    :sparkles: Added PostConstruct annotation to createFolders method in IOConfigImpl.kt
    This commit includes a refactor on the `IOConfigImpl.kt` file in the `nico2vocadb-songlist-exporter` module. The `createFolders()` method, which was previously called in the initialization block, now has a `@PostConstruct` annotation.
    
    The `@PostConstruct` annotation is used to ensure that the method it is annotated with gets executed after dependency injection is done to perform any initialization. This means that the `createFolders()` method will be automatically invoked after the spring bean is initialized.
    
    Also, the `createFolders()` method now overrides a method from its superclass. The `super.createFolders()` call has been added to the method body to maintain the original behavior.

--- app/nico2vocadb-songlist-exporter/src/main/kotlin/mikufan/cx/vtool/app/n2vex/config/IOConfigImpl.kt
+++ app/nico2vocadb-songlist-exporter/src/main/kotlin/mikufan/cx/vtool/app/n2vex/config/IOConfigImpl.kt
@@ -1,5 +1,6 @@
 package mikufan.cx.vtool.app.n2vex.config
 
+import jakarta.annotation.PostConstruct
 import mikufan.cx.vtool.core.n2vex.config.IOConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.validation.annotation.Validated
@@ -12,7 +13,9 @@ data class IOConfigImpl(
   override val outputCsv: Path,
   override val notFoundCsv: Path
 ) : IOConfig {
-  init {
-    createFolders()
+
+  @PostConstruct
+  override fun createFolders() {
+    super.createFolders()
   }
 }
