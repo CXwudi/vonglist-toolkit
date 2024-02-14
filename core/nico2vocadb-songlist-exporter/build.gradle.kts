b2c56b3fd615b42e497b54eaf2716b50dca488c1
CXwudi <cx2010017@gmail.com>
2024-02-13 22:13:19 -0500

    :arrow_up_down: Dependency modifications in build files
    This commit brings changes to the dependencies in the build files of two modules: `nico2vocadb-songlist-exporter` and `vocadb-songlist-importer`.
    
    In `nico2vocadb-songlist-exporter` module, we have removed the `spring-boot-dependencies` from the API platform. This change has been made to streamline the dependencies of the project and remove unnecessary ones.
    
    On the other hand, we have added `kotlin-jvm-inline-logging` dependency in `vocadb-songlist-importer` module. This additional logging dependency will help in providing more comprehensive logging for the module and enhance the debugging process.
    
    Please note, these changes are intended to optimize the project dependencies and improve the overall project structure.

--- core/nico2vocadb-songlist-exporter/build.gradle.kts
+++ core/nico2vocadb-songlist-exporter/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
   implementation(project(":shared:all-model"))
-  api(platform("org.springframework.boot:spring-boot-dependencies"))
   api(project(":component:io-api"))
   api(project(":component:http-service-api"))
   api("com.github.CXwudi:kotlin-jvm-inline-logging")
--- core/vocadb-songlist-importer/build.gradle.kts
+++ core/vocadb-songlist-importer/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
 
 dependencies {
   implementation(project(":shared:all-model"))
+  api("com.github.CXwudi:kotlin-jvm-inline-logging")
 }
