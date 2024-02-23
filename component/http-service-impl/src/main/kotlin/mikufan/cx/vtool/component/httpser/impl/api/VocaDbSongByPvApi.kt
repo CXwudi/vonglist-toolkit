b253ea8a2f22f264e8259a6366f9828d7c590a15
CXwudi <cx2010017@gmail.com>
2024-02-23 17:11:32 -0500

    :memo: documentation

--- component/http-service-impl/src/main/kotlin/mikufan/cx/vtool/component/httpser/impl/api/VocaDbSongByPvApi.kt
+++ component/http-service-impl/src/main/kotlin/mikufan/cx/vtool/component/httpser/impl/api/VocaDbSongByPvApi.kt
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 
+/**
+ * Interface for interacting with the VocaDB API.
+ * Provides methods for fetching songs by PV.
+ */
 @HttpExchange("/api")
 interface VocaDbSongByPvApi {
 
+  /**
+   * Fetches a song by its PV.
+   * @param pvId The ID of the PV.
+   * @param pvService The service of the PV.
+   * @return The song as a JsonNode, or null if not found.
+   */
   @GetExchange("/songs/byPv")
   fun getSongByPv(
     @RequestParam pvId: String,
