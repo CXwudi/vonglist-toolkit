a67abd18c5ba79b99e30940394ea5e793bf4869c
CXwudi <cx2010017@gmail.com>
2024-02-24 18:00:53 -0500

    :recycle: Refactored IOConfigImpl and FileReadableValidator
    This commit introduces several changes to the IOConfigImpl.kt file. It began with updating the import statements to include `kotlin.io.path.isReadable` and `kotlin.io.path.isRegularFile`.
    
    In the IOConfigImpl data class, the annotation `@FileReadable` was moved from the field `inputCsv` to its getter method, thus changing the target of the annotation from the field to the getter. This change allows the annotation to be applied to the returned value of the getter method, instead of the field itself.
    
    The `@FileReadable` annotation was also updated. Besides changing its target, two more changes were made. The `@MustBeDocumented` annotation was added, ensuring that the `@FileReadable` annotation will be included in the Kotlin Doc. The `@Retention` attribute was updated from `AnnotationRetention.RUNTIME` to `AnnotationRetention.SOURCE`. This change means that the annotation will be discarded by the compiler and won't be present in the compiled class files.
    
    The FileReadableValidator class, which is used to validate the `@FileReadable` annotation, was refactored. The file readability check was updated from `Files.isReadable(path)` to `path.isReadable()`, using the `isReadable()` extension function from `kotlin.io.path`. This change simplifies the code and makes it more idiomatic to Kotlin. Furthermore, the call to `addPropertyNode("inputCsv")` was removed, as it is no longer necessary after the refactoring.

--- app/vocadb-songlist-importer/src/main/kotlin/mikufan/cx/vtool/app/vsli/config/IOConfigImpl.kt
+++ app/vocadb-songlist-importer/src/main/kotlin/mikufan/cx/vtool/app/vsli/config/IOConfigImpl.kt
@@ -8,8 +8,8 @@ import jakarta.validation.constraints.Positive
 import mikufan.cx.vtool.core.vsli.config.IOConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.validation.annotation.Validated
-import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.isReadable
 import kotlin.reflect.KClass
 
 @ConfigurationProperties(prefix = "io")
@@ -17,13 +17,14 @@ import kotlin.reflect.KClass
 data class IOConfigImpl(
   @get:Positive
   override val vocaDbListId: Long?,
-  @FileReadable
+  @get:FileReadable
   override val inputCsv: Path,
 ) : IOConfig
 
 @Constraint(validatedBy = [FileReadableValidator::class])
-@Target(AnnotationTarget.FIELD)
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER)
 @Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
 annotation class FileReadable(
   val message: String = "The input csv file must be readable",
   val groups: Array<KClass<*>> = [],
@@ -33,12 +34,11 @@ annotation class FileReadable(
 
 class FileReadableValidator : ConstraintValidator<FileReadable, Path> {
   override fun isValid(path: Path, context: ConstraintValidatorContext): Boolean {
-    return if (Files.isReadable(path)) {
+    return if (path.isReadable()) {
       true
     } else {
       context.disableDefaultConstraintViolation()
       context.buildConstraintViolationWithTemplate("The file $path is not readable")
-        .addPropertyNode("inputCsv")
         .addConstraintViolation()
       false
     }
