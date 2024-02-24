2481c4584df85794814b69270293b1f7044e3cae
CXwudi <cx2010017@gmail.com>
2024-02-24 18:02:05 -0500

    :test_tube: **Add integration test and test configuration for VocaDbSongListImporter**
    This commit introduces an integration test for `VocaDbSongListImporter` (`VocaDbSongListImporterIT.kt`). This test validates that the `MainImporterWithLocalRead` function runs without throwing an exception.
    
    The test utilizes `SpringExtension` and is run under the 'test' profile. This ensures that the test environment correctly mirrors the application environment.
    
    Additionally, this commit adds a new configuration file (`application-test.yml`) for the test environment which includes settings for input/output CSV files and a VocaDb list id. It also includes configuration for HTTP cookies and debug level logging for HTTP headers. This configuration ensures the test environment can properly interact with VocaDb and its HTTP interfaces.
    
    With these changes, we can now perform integration testing on the `VocaDbSongListImporter` functionality, which helps ensure its reliability and correctness.

---
+++ app/vocadb-songlist-importer/src/test/kotlin/mikufan/cx/vtool/app/vsli/VocaDbSongListImporterIT.kt
@@ -0,0 +1,25 @@
+package mikufan.cx.vtool.app.vsli
+
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.extensions.spring.SpringExtension
+import mikufan.cx.vtool.core.vsli.MainImporterWithLocalRead
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class VocaDbSongListImporterIT(
+  private val mainImporterWithLocalRead: MainImporterWithLocalRead,
+) : ShouldSpec({
+
+  xcontext("main importer") {
+    should("run") {
+      shouldNotThrow<Exception> {
+        mainImporterWithLocalRead.run()
+      }
+    }
+  }
+}) {
+  override fun extensions() = listOf(SpringExtension)
+}
