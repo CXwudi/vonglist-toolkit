33cdf17f97ee051b25844555344ff24a235b639a
CXwudi <cx2010017@gmail.com>
2024-04-30 12:37:44 -0400

    📝 `NicoListFetcher` interface with detailed documentation
    This commit enhances the `NicoListFetcher` interface in the HTTP service API module by adding comprehensive documentation and refining the method for fetching song lists from NicoNico. The updated documentation clearly outlines the purpose and usage of the interface, providing insights into its functionality and the expected behavior of its implementation. The `readAllSongsFromList` method now includes detailed parameter descriptions, making the API more accessible and easier to understand for developers. This update ensures that the interface not only meets functional requirements but also adheres to best practices in code documentation and design clarity.

--- component/http-service-api/src/main/kotlin/mikufan/cx/vtool/component/httpser/api/NicoListFetcher.kt
+++ component/http-service-api/src/main/kotlin/mikufan/cx/vtool/component/httpser/api/NicoListFetcher.kt
@@ -3,7 +3,26 @@ package mikufan.cx.vtool.component.httpser.api
 import mikufan.cx.vtool.shared.model.niconico.NicoListItem
 import mikufan.cx.vtool.shared.model.niconico.NicoListSortPreference
 
+/**
+ * Interface for fetching song lists from NicoNico.
+ *
+ * Provides a method for reading all songs from a list given its ID and a sort preference.
+ *
+ * The implementation of this interface should lazily read the list page by page using NicoNico API,
+ * and should return an iterator of `NicoListItem` which represents the songs in the list.
+ *
+ * @see NicoListItemIterator
+ * @see NicoListItem
+ * @see NicoListSortPreference
+ */
 interface NicoListFetcher {
+  /**
+   * Reads all songs from a list on NicoNico given its ID and a sort preference.
+   *
+   * @param id The ID of the song list.
+   * @param sortPreference The preference for sorting the songs in the list.
+   * @return An iterator of `NicoListItem` which represents the songs in the list.
+   */
   fun readAllSongsFromList(
     id: Long,
     sortPreference: NicoListSortPreference,
